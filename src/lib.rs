e91ef7290856e63e1ed705d3f957e673538de4cf
Sean Stangl <sstangl@mozilla.com>
2017-12-13 12:23:03 -0800

    Add bitops-nsieve-bits (untested).

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+#![feature(iterator_step_by)]
+
 mod bitops_bitwise_and;
 mod bitops_3bit_bits_in_byte;
+mod bitops_nsieve_bits;
 
 pub use bitops_bitwise_and::*;
 pub use bitops_3bit_bits_in_byte::*;
+pub use bitops_nsieve_bits::*;
