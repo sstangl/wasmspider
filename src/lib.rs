e25ae9773437befad1cdeaab5bf24350e43bd6ae
Sean Stangl <sstangl@mozilla.com>
2017-12-13 12:35:59 -0800

    Add controlflow-recursive.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 mod bitops_bitwise_and;
 mod bitops_3bit_bits_in_byte;
 mod bitops_nsieve_bits;
+mod controlflow_recursive;
 
 pub use bitops_bitwise_and::*;
 pub use bitops_3bit_bits_in_byte::*;
 pub use bitops_nsieve_bits::*;
+pub use controlflow_recursive::*;
