94d8bb3b6a6edc7b3f6f235253e50e938f6c55a7
Sean Stangl <sstangl@mozilla.com>
2017-12-13 20:26:42 -0800

    Add math-partial-sums.

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,11 @@ mod bitops_bitwise_and;
 mod bitops_3bit_bits_in_byte;
 mod bitops_nsieve_bits;
 mod controlflow_recursive;
+mod math_partial_sums;
 
 pub use access_binary_trees::*;
 pub use bitops_bitwise_and::*;
 pub use bitops_3bit_bits_in_byte::*;
 pub use bitops_nsieve_bits::*;
 pub use controlflow_recursive::*;
+pub use math_partial_sums::*;
